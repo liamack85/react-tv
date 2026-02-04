b782cc04243f837e258469461a8ef8ba0233d8e7
liamack <liamack85@gmail.com>
2026-02-04 11:36:37 -0500

    adding notes

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,21 +1,32 @@
-import { useState } from "react";
+import { useState } from "react"; /**Create once usetState() is called */
 import EpisodeList from "../episodes/EpisodeList";
 import EpisodeDetails from "../episodes/EpisodeDetails";
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails({ show }) {
-  const [selectedEpisode, setSelectedEpisode] = useState();
+  /**<ShowDetails> takes a show as a prop. */
+  const [selectedEpisode, setSelectedEpisode] =
+    useState(); /**<ShowDetails> keeps the selected episode in a state variable. */
   if (!show) {
-    return <p>Select a show to get started!</p>;
-  }
+    /**If no show is provided, <ShowDetails> renders a message encouraging the user to select a show. */
+    return (
+      <p>Select a show to get started!</p>
+    ); /**In react land, render means return */
+  } /**This is a guard case */
   return (
     <div className="show-details">
-      <EpisodeList
+      <EpisodeList /**If a show is provided, <ShowDetails> renders <EpisodeList> and <EpisodeDetails>. */
         name={show.name}
-        episodes={show.episodes}
-        selectedEpisode={selectedEpisode}
-        setSelectedEpisode={setSelectedEpisode}
+        episodes={
+          show.episodes
+        } /**<EpisodeList> and <EpisodeDetails> are provided the correct props. Read the source code of these components to figure out what props they expect! */
+        selectedEpisode={
+          selectedEpisode
+        } /**When you are passing in a prop (from EpisodeList in this case) */
+        setSelectedEpisode={
+          setSelectedEpisode
+        } /**The left side of the = must match up to the prop name */
       />
       <EpisodeDetails episode={selectedEpisode} />
     </div>
